5ec9389347c91108e61a3c89cca58f7f1dec4e76
Leyson Castaño <leysofcp@hotmail.com>
2025-12-31 12:57:16 -0500

    Update Tema2.vue

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -221,7 +221,7 @@
 
     figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/N3oOeboqWvg?si=oycbofuDH3bo3WiB" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
 
     .titulo-con-imagen-2.mb-5(data-aos="fade-right")
